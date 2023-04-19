fa5a04f658efb9e57e57a82d386f4c692f9a6fb6
jordanmodic <118031821+jordanmodic@users.noreply.github.com>
2023-04-19 12:02:00 -0700

    Added jQuery CDN and function
    Added CDN and fadeIn and fadeOut functions for the case study buttons

--- js/index.js
+++ js/index.js
@@ -1,2 +1,8 @@
 
 console.log("Your index.js file is loaded correctly!");
+$(".case-study-content").mouseover(function(){
+    $(".button").fadeIn();
+});
+$(".case-study-content").mouseleave(function(){
+    $(".button").fadeOut();
+})
